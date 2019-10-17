53326a069829af99817c5f7233b6c44c2c5fea04
FreeCoder-280 <56636678+FreeCoder-280@users.noreply.github.com>
2019-10-17 12:09:00 +0200

    Update space_view3d_align_faces.py

--- space_view3d_align_faces.py
+++ space_view3d_align_faces.py
@@ -1,11 +1,3 @@
-#------------------------------------------------------------------------------
-# Copyright © 2019 FreeCoder-280 (anonymous works :-)
-# This program is free software. It comes without any warranty, to
-# the extent permitted by applicable law. You can redistribute it
-# and/or modify it under the terms of the Do What The Fuck You Want
-# To Public License, Version 2, as published by Sam Hocevar.
-# See http://www.wtfpl.net/ for more details.
-#------------------------------------------------------------------------------
 import bpy
 import math
 from mathutils import Vector
